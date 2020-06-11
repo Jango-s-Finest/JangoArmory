63c288c23c827222e08b7438f6d71b53866ffd88
icedraikon <66761254+icedraikon@users.noreply.github.com>
2020-06-11 02:23:11 -0400

    JTAC
    Imported JTAC and IQ from the Trello, and added JTAC into the mod. Couldn't figure out how to get JangosArmory4

--- JangosArmory3/config.cpp
+++ JangosArmory3/config.cpp
@@ -69,6 +69,41 @@ class CfgWeapons
 	class VestItem;
 	
 	// General Uniforms
+	class JA_104th_JTAC_Uniform : SWLB_clone_uniform
+	{
+		author = "Ice";
+		scope = 2;
+		allowedSlots[] = { BACKPACK_SLOT };
+		displayName = "Clone trooper armor (104th JTAC)";
+		hiddenSelections[] =
+		{
+			"camo1",
+			"camo2"
+		};
+		hiddenSelectionsTextures[] =
+		{
+			"JangosArmory3\data\Textures\104th_JTAC_Base_Upper.paa",
+			"JangosArmory3\data\Textures\104th_JTAC_Base_Lower.paa"
+		};
+		class ItemInfo : UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "JA_104th_JTAC";
+			containerClass = "Supply150";
+			mass = 40;
+			uniformType = "Neopren";
+		};
+	};
+	class JA_104th_JTAC_Helmet : SWLB_P2_SpecOps_Helmet
+	{
+		author = "Ice";
+		scopeArsenal = 2;
+        side = 1;
+		grad_slingHelmet_allow = true;
+		hiddenSelections[] = { "camo1" }; // don't change this
+		displayname = "Clone Trooper Special Activities Helmet (104th JTAC)"; // the name it will be in game
+		hiddenSelectionsTextures[] = { "JangosArmory3\data\Textures\104th_JTAC_Base_Helmet.paa" }; // the file path to the texture
+	};
 	class JA_104th_Crowbi_Helmet : SWLB_clone_P15_Helmet
 	{
 		author = "Ice";
@@ -808,6 +843,21 @@ class CfgVehicles
 	class SWLB_clone_backpack_medic;
 	class SWLB_clone_backpack_RTO;
 	// General Uniforms
+	class JA_104th_JTAC : SWLB_clone_base_P2
+	{
+		author = "Ice"
+		scope = 2;
+		scopeArsenal = 2;
+        side = 1;
+		uniformClass = "JA_104th_JTAC_Uniform";
+		displayName = "104th Base JTAC";
+		faction = "104th_Guys";
+		editorSubcategory = "104th_categ_clones";
+		hiddenSelections[] = { "camo1", "camo2" };
+		hiddenSelectionsTextures[] = {"JangosArmory3\data\Textures\104th_JTAC_Base_Upper.paa", "JangosArmory3\data\Textures\104th_JTAC_Base_Lower.paa"};
+		linkedItems[] = { JA_104th_JTAC_Helmet,SWLB_clone_basic_armor, ItemMap, ItemCompass, ItemWatch, ItemGPS, ItemRadio }; // all items that will be on unit
+		respawnLinkedItems[] = { JA_104th_JTAC_Helmet,SWLB_clone_basic_armor, ItemMap, ItemCompass, ItemWatch, ItemGPS, ItemRadio }; // all items that will be on unit on respawn
+	};	
 	class JA_104th_Crowbi : SWLB_clone_base_P2
 	{
 		author = "Ice"
