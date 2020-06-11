c550a517aa6fde0aaeea04a818a5a6bf67e6f425
icedraikon <66761254+icedraikon@users.noreply.github.com>
2020-06-11 00:16:20 -0400

    Warpath

--- JangosArmory3/config.cpp
+++ JangosArmory3/config.cpp
@@ -69,6 +69,16 @@ class CfgWeapons
 	class VestItem;
 	
 	// General Uniforms
+	class JA_104th_Warpath_Helmet : SWLB_clone_AB_helmet
+	{
+		author = "Dak";
+		scopeArsenal = 2;
+        side = 1;
+		grad_slingHelmet_allow = true;
+		hiddenSelections[] = { "camo1" }; // don't change this
+		displayname = "Clone Trooper Airborne Helmet (104th Warpath)"; // the name it will be in game
+		hiddenSelectionsTextures[] = { "JangosArmory3\data\Textures\104th_AB_Warpath_Helmet.paa" }; // the file path to the texture
+	};
 	class JA_104th_Waffle_Helmet : SWLB_clone_AB_helmet
 	{
 		author = "Dak";
