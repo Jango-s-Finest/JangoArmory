f9c6bc8463c12486e696678bef6b4564e21ba50c
icedraikon <66761254+icedraikon@users.noreply.github.com>
2020-06-11 01:09:18 -0400

    Bravo

--- JangosArmory/config.cpp
+++ JangosArmory/config.cpp
@@ -538,7 +538,7 @@ class CfgWeapons
 		hiddenSelectionsTextures[] = { "JangosArmory\data\Textures\104th_AB_Habit_Helmet.paa" }; // the file path to the texture
 	};
 
-	class JA_104th_Bravo_Helmet : SWLB_clone_AB_helmet
+	class JA_104th_Bravo_Helmet : SWLB_Clone_Pilot_P2_Helmet
 	{
 		author = "Emmet";
 		scopeArsenal = 2;
@@ -546,7 +546,7 @@ class CfgWeapons
 		grad_slingHelmet_allow = true
 			hiddenSelections[] = { "camo1" }; // dont change thise
 		displayName = "Clone Airborne Helmet (104th Bravo)"; // the name it will be in game
-		hiddenSelectionsTextures[] = { "JangosArmory\data\Textures\104th_Bravo_AB_Helmet.paa" }; // The file path to the texture
+		hiddenSelectionsTextures[] = { "JangosArmory\data\Textures\104th_Bravo_Helmet.paa" }; // The file path to the texture
 	};
 
 	// Pilots Helmets
