4918d06fda5fbb77bef4ddc9450f333ee826255e
nicholas <nicbrant@gmail.com>
2020-06-10 23:04:20 -0300

    upload

---
+++ JangosArmory4/config.cpp
@@ -0,0 +1,85 @@
+#include "basicDefines_A3.hpp"
+class DefaultEventhandlers;
+class UniformSlotInfo;
+class CfgPatches
+{
+	class Jangos_Armory
+	{
+		author = "Jango's Finest";
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgFactionClasses
+{
+	class 104th_Guys
+	{
+		displayname = "Jango's Finest";
+		priority = 1; // Position in list. 
+        side = 1; // Opfor = 0, Blufor = 1, Indep = 2. 
+	};
+};
+class cfgEditorSubcategories
+{
+	class 104th_Categ_Clones
+	{
+		displayname = "104th - Customs";
+	};
+	class 104th_Categ_Basic
+	{
+		displayname = "104th - Base";
+	};
+	class 104th_Categ_Special
+	{
+		displayname = "104th - Special";
+	};
+};
+
+class CfgWeapons
+{
+	class InventoryItem_Base_F;
+	class ItemCore;
+	class SWLB_Clone_Pilot_P2_Helmet;
+	class SWLB_clone_uniform;
+	class SWLB_clone_mc_uniform;
+	class SWLB_clone_arc_armor;
+	class SWLB_clone_officer_armor;
+	class SWLB_clone_commander_armor;
+	class SWLB_Clone_airborne_armor;
+	class SWLB_CEE_Airborne_Officer;
+	class SWLB_CEE_Force_Recon_NCO;
+	class SWLB_clone_BARC_helmet;
+	class SWLB_clone_AB_helmet;
+	class SWLB_clone_ARF_P1_Helmet;
+	class SWLB_clone_P15_Helmet;
+	class SWLB_clone_P1_helmet;
+	class SWLB_clone_P1_2_helmet;
+	class SWLB_clone_P2_Helmet;
+	class SWLB_clone_nvg;
+	class SWLB_clone_nvg_nco;
+	class SWLB_clone_ccVisor;
+	class SWLB_clone_mcVisor;
+	class SWLB_clone_kama_armor;
+	class SWLB_clone_medic_armor;
+	class SWLB_clone_basic_armor;
+	class SWLB_clone_airborne_nco_armor;
+	class UniformItem;
+	class VestItem;
+	
+	// General Uniforms
+	
+};
+
+class CfgVehicles
+{
+	class SWLB_clone_base_P2;
+	class SWLB_clone_marshal_commander_base_P2;
+	class SWLB_clone_backpack;
+	class SWLB_clone_RTO_mini_backpack;
+	class SWLB_clone_backpack_heavy;
+	class SWLB_clone_backpack_medic;
+	class SWLB_clone_backpack_RTO;
+	// General Uniforms
+	
+};
