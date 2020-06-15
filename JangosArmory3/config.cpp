ff3b38c0eca0ab07982cf4dd22005ebdfb0fa139
icedraikon <66761254+icedraikon@users.noreply.github.com>
2020-06-14 21:46:07 -0400

    Ice Uniform
    Tried to fix the rank plate on the uniform. Used my uniform as the experimental monkey. Didn't work. I also can't find the rank plate in the texture map, so i have no idea

--- JangosArmory3/config.cpp
+++ JangosArmory3/config.cpp
@@ -606,16 +606,12 @@ class CfgWeapons
 		hiddenSelections[] =
 		{
 			"camo1",
-			"camo2",
-			"camo3",
-			"camo4"
+			"camo2"
 		};
 		hiddenSelectionsTextures[] =
 		{
 			"JangosArmory3\data\Textures\104th_ARF_Ice_Upper.paa",
 			"JangosArmory3\data\Textures\104th_ARF_Ice_Lower.paa",
-			"JangosArmory3\data\Textures\104th_ARF_Ice_Upper.paa",
-			""
 		};
 		class ItemInfo : UniformItem
 		{
@@ -990,8 +986,8 @@ class CfgVehicles
 		displayName = "104th Arf Ice";
 		faction = "104th_Guys";
 		editorSubcategory = "104th_categ_clones";
-		hiddenSelections[] = { "camo1", "camo2","camo3","camo4" };
-		hiddenSelectionsTextures[] = {"JangosArmory3\data\Textures\104th_ARF_Ice_Upper.paa", "JangosArmory3\data\Textures\104th_ARF_Ice_Lower.paa","JangosArmory3\data\Textures\104th_ARF_Ice_Upper.paa",""};
+		hiddenSelections[] = { "camo1", "camo2" };
+		hiddenSelectionsTextures[] = {"JangosArmory3\data\Textures\104th_ARF_Ice_Upper.paa", "JangosArmory3\data\Textures\104th_ARF_Ice_Lower.paa"};
 		linkedItems[] = { JA_104th_ARF_Ice_Helmet,SWLB_clone_basic_armor, ItemMap, ItemCompass, ItemWatch, ItemGPS, ItemRadio }; // all items that will be on unit
 		respawnLinkedItems[] = { JA_104th_ARF_Ice_Helmet,SWLB_clone_basic_armor, ItemMap, ItemCompass, ItemWatch, ItemGPS, ItemRadio }; // all items that will be on unit on respawn
 	};	
