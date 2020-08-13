8285717ccab454a7ce5614cf2b4eb1b2fd073afe
nicholas <nicbrant@gmail.com>
2020-08-13 15:38:47 -0300

    other droids

--- JangosArmory_CIS/config.cpp
+++ JangosArmory_CIS/config.cpp
@@ -9,7 +9,13 @@ class CfgPatches
 		requiredVersion = 0.1;
 		requiredAddons[] = {};
 		units[] = {
-			"JA_CIS_B2"
+			"JA_CIS_BX_Assassin",
+			"JA_CIS_BX_Commando",
+			"JA_CIS_B1_Marksman",
+			"JA_CIS_B1_AA",
+			"JA_CIS_B1_Heavy",
+			"JA_CIS_B1_Base",
+			"JA_CIS_B2_Base"
 		};
 		weapons[] = {
 			"JA_CIS_B2_Uniform"
@@ -50,188 +56,67 @@ class CfgWeapons
 		displayName = "Droid B2";
 		hiddenSelections[] = {"Camo1","Camo2","Camo3"};
 		hiddenSelectionsTextures[] = {"RD501_Units\textures\CIS\B2\rocket\Reskinb2_chest.paa","RD501_Units\textures\CIS\B2\rocket\ReskinB2_waist.paa","RD501_Units\textures\CIS\B2\rocket\ReskinB2_legs.paa"};
-		class HitPoints
-		{
-			class HitArms{
-				armor = 10;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "arms";
-				passThrough = 0.1;
-				radius = 0.1;
-				visual = "injury_hands";
-			};
-			class HitHands: HitArms{
-				armor = 11;
-				depends = "HitArms";
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "hands";
-				passThrough = 0.1;
-				radius = 0.1;
-				visual = "injury_hands";
-			};
-			class HitLeftArm{
-				armor = 1;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "hand_l";
-				passThrough = 0.1;
-				radius = 0.1;
-				visual = "injury_hands";
-			};
-			class HitLeftLeg{
-				armor = 1;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "leg_l";
-				passThrough = 0.1;
-				radius = 0.1;
-				visual = "injury_hands";
-			};
-			class HitLegs{
-				armor = 10;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "legs";
-				passThrough = 0.1;
-				radius = 0.12;
-				visual = "injury_legs";
-			};
-			class HitFace{
-				armor = 3;
-				explosionShielding = 0.1;
-				material = -1;
-				minimalHit = 0.01;
-				name = "face_hub";
-				passThrough = 0.1;
-				radius = 0.08;
-			};
-			class HitNeck: HitFace{
-				armor = 5;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "neck";
-				passThrough = 0.1;
-				radius = 0.1;
-			};
-			class HitHead: HitNeck{
-				armor = 10;
-				depends = "HitFace max HitNeck";
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "head";
-				passThrough = 0.2;
-				radius = 0.15;
-			};
-			class ACE_HDBracket:HitHead{
-				armor = 1;
-				depends = "HitHead";
-				explosionShielding = 1;
-				material = -1;
-				minimalHit = 0;
-				name = "head";
-				passThrough = 0;
-				radius = 1;
-				visual = "";
-			};
-			class HitPelvis{
-				armor = 10;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "pelvis";
-				passThrough = 0.1;
-				radius = 0.2;
-				visual = "injury_body";
-			};
-			class HitAbdomen: HitPelvis{
-				armor = 10;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "spine1";
-				passThrough = 0.1;
-				radius = 0.15;
-				visual = "injury_body";
-			};
-			class HitDiaphragm: HitAbdomen{
-				armor = 8;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "spine2";
-				passThrough = 0.1;
-				radius = 0.15;
-				visual = "injury_body";
-			};
-			class HitChest: HitDiaphragm{
-				armor = 8;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "spine3";
-				passThrough = 0.1;
-				radius = 0.15;
-				visual = "injury_body";
-			};
-			class HitBody: HitChest{
-				armor = 10;
-				depends = "HitPelvis max HitAbdomen max HitDiaphragm max HitChest";
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "body";
-				passThrough = 0.1;
-				radius = 0.16;
-				visual = "injury_body";
-			};
-			class HitRightArm{
-				armor = 1;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "hand_r";
-				passThrough = 0.1;
-				radius = 0.1;
-				visual = "injury_hands";
-			};
-			class HitRightLeg{
-				armor = 1;
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "leg_r";
-				passThrough = 0.1;
-				radius = 0.1;
-				visual = "injury_hands";
-			};
-			class Incapacitated{
-				armor = 10;
-				depends = "(((Total - 0.25) max 0) + ((HitHead - 0.25) max 0) + ((HitBody - 0.25) max 0)) * 2";
-				explosionShielding = 0.25;
-				material = -1;
-				minimalHit = 0.01;
-				name = "body";
-				passThrough = 0.1;
-				radius = 0;
-				visual = "";
-			};
-		};
 	};
 };
 
 class CfgVehicles
 {
 	class RD501_opfor_uniform_skin_B2_Armor;
-	class JA_CIS_B2:RD501_opfor_uniform_skin_B2_Armor{
+	class SWLB_b1_base;
+	class SWLB_b1_heavy_base;
+	class SWLB_b1_AA_base;
+	class SWLB_b1_marksman_base;
+	class SWLB_BX_Assassin;
+	class SWLB_BX_Commando;
+	class JA_CIS_BX_Assassin: SWLB_BX_Assassin{
+		scope = 2;
+		scopeArsenal = 2;
+		scopeCurator = 2;
+		side = 0;
+		faction = "104th_Guys_CIS";
+		editorSubcategory = "104th_Categ_CIS";
+	};
+	class JA_CIS_BX_Commando: SWLB_BX_Commando{
+		scope = 2;
+		scopeArsenal = 2;
+		scopeCurator = 2;
+		side = 0;
+		faction = "104th_Guys_CIS";
+		editorSubcategory = "104th_Categ_CIS";
+	};
+	class JA_CIS_B1_Marksman: SWLB_b1_marksman_base{
+		scope = 2;
+		scopeArsenal = 2;
+		scopeCurator = 2;
+		side = 0;
+		faction = "104th_Guys_CIS";
+		editorSubcategory = "104th_Categ_CIS";
+	};
+	class JA_CIS_B1_AA: SWLB_b1_AA_base{
+		scope = 2;
+		scopeArsenal = 2;
+		scopeCurator = 2;
+		side = 0;
+		faction = "104th_Guys_CIS";
+		editorSubcategory = "104th_Categ_CIS";
+	};
+	class JA_CIS_B1_Heavy: SWLB_b1_heavy_base{
+		scope = 2;
+		scopeArsenal = 2;
+		scopeCurator = 2;
+		side = 0;
+		faction = "104th_Guys_CIS";
+		editorSubcategory = "104th_Categ_CIS";
+	};
+	class JA_CIS_B1_Base: SWLB_b1_base{
+		scope = 2;
+		scopeArsenal = 2;
+		scopeCurator = 2;
+		side = 0;
+		faction = "104th_Guys_CIS";
+		editorSubcategory = "104th_Categ_CIS";
+	};
+	class JA_CIS_B2_Base: RD501_opfor_uniform_skin_B2_Armor{
 		author = "Dak"
 		scope = 2;
 		scopeArsenal = 2;
@@ -359,7 +244,7 @@ class CfgVehicles
 				visual = "injury_body";
 			};
 			class HitDiaphragm: HitAbdomen{
-				armor = 8;
+				armor = 10;
 				explosionShielding = 0.25;
 				material = -1;
 				minimalHit = 0.01;
@@ -369,7 +254,7 @@ class CfgVehicles
 				visual = "injury_body";
 			};
 			class HitChest: HitDiaphragm{
-				armor = 8;
+				armor = 10;
 				explosionShielding = 0.25;
 				material = -1;
 				minimalHit = 0.01;
