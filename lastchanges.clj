677890001b4668cf77ef6d6a6b5976893cc2b759
Nicholas Brant <nicbrant@gmail.com>
2025-05-04 22:06:58 -0300

    changelog: lastchanges.clj

--- lastchanges.clj
+++ lastchanges.clj
@@ -1,27 +1,14 @@
 [h2]Custom Gear[/h2]
 [h3]Added[/h3]
 [list]
-    [*] Clone Trooper AB Helmet (104th Scrub)
-    [*] Clone Commander Visor (104th Sigil)
-    [*] 15S UGL
-    [*] 3AS rounds to DP23
-    [*] 15C ACOG scope to A/C
-    [*] bipod to 15A
 [/list]
 [h3]Removed[/h3]
 [list]
-    [*] Removed DW32S
 [/list]
 [h3]Changes[/h3]
 [list]
-    [*] Clone Trooper Engineer Helmet (104th Archibold)
-    [*] Clone Trooper armor (104th Archibold)
-    [*] Clone Trooper armor (104th Achilles)
-    [*] Clone ARC Trooper Vest (104th Sigil)
-    [*] Clone ARC Trooper Vest (104th Vision)
-    [*] Clone Trooper ARC Trooper Helmet (104th Vision)
-    [*] Clone Trooper armor (104th Vision)
-    [*] Clone Trooper armor (104th Sigil)
-    [*] 30MW 50Rnd mags to 15A in place of standard capacity mags
-    [*] Reduced RPM on 15A
+    [*] 3AS SFX for 15X, and LS for 17M
+    [*] Removed broken holoscopes from 15S and added 3AS scopes in place of removed ones
+    [*] Removed sniper mags and added standard mags to 15A
+    [*] SF pistol mag reduced from 100 rnds to 40
 [/list]
